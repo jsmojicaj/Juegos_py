ad6299a33a26857b97c2b5f8c131f3cb863477c5
Juan Sebastian Mojica <jsmojicaj@unal.edu.co>
2023-04-06 11:24:57 -0500

    Se anuncia quién gano

--- piedra,papel o tijera.py
+++ piedra,papel o tijera.py
@@ -1,6 +1,7 @@
 import random
 nombre=input("Escoge: piedra, papel o tijera: ")
 print(f"has escogido {nombre}")
+
 PC=random.randint(1,3)
 if PC==1:
     PC_choise="piedra"
@@ -8,13 +9,15 @@ elif PC==2:
     PC_choise="papel"
 elif PC==3:
     PC_choise="tijera"
-print(f"El pc escogio {PC}:{PC_choise} \n")
+if (nombre!="piedra") or (nombre!="papel") or (nombre!="tijera"):
+    print("ERROR!")
 
-if nombre=="papel":
-    print("papel gana piedra")
-elif nombre=="piedra":
-    print("Piedra gana tijera")
-elif nombre=="tijera":
-    print("tijera gana papel")
+print(f"Escogiste {nombre}")
+print(f"El pc escogio {PC_choise} \n")
+
+if nombre==PC_choise:
+    print("Es empate!")
+elif (nombre=="piedra" and PC_choise=="tijera") or (nombre=="tijera" and PC_choise=="papel") or (nombre=="papel" and PC_choise=="piedra"):
+    print("Has ganado!")
 else:
-    print("ERROR! por favor,escoge: piedra, papel o tijera ")
+    print("Has perdido!")
