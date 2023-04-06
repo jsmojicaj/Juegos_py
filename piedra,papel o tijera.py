4a06ac2ab143f868e875a5fce252b9bee7887c9e
Juan Sebastian Mojica <jsmojicaj@unal.edu.co>
2023-04-06 11:08:12 -0500

    Ahora el PC puede escoger aleatoriamente una de las tres opciones

--- piedra,papel o tijera.py
+++ piedra,papel o tijera.py
@@ -1,6 +1,20 @@
-print("Escoge, piedra, papel o tijera")
-nombre=input()
+import random
+nombre=input("Escoge: piedra, papel o tijera: ")
+print(f"has escogido {nombre}")
+PC=random.randint(1,3)
+if PC==1:
+    PC_choise="piedra"
+elif PC==2:
+    PC_choise="papel"
+elif PC==3:
+    PC_choise="tijera"
+print(f"El pc escogio {PC}:{PC_choise} \n")
+
 if nombre=="papel":
-    print("ganaste")
+    print("papel gana piedra")
+elif nombre=="piedra":
+    print("Piedra gana tijera")
+elif nombre=="tijera":
+    print("tijera gana papel")
 else:
-    print("perdiste")
+    print("ERROR! por favor,escoge: piedra, papel o tijera ")
