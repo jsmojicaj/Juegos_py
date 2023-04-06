c136f15dd131aa2886bd597ea969496bba74c6ec
Juan Sebastian Mojica <jsmojicaj@unal.edu.co>
2023-04-06 11:57:22 -0500

    Corrección por si el usuario comete algún error

--- piedra,papel o tijera.py
+++ piedra,papel o tijera.py
@@ -1,6 +1,5 @@
 import random
-nombre=input("Escoge: piedra, papel o tijera: ")
-print(f"has escogido {nombre}")
+nombre=input("Escoge: piedra, papel o tijera => ")
 
 PC=random.randint(1,3)
 if PC==1:
@@ -9,10 +8,19 @@ elif PC==2:
     PC_choise="papel"
 elif PC==3:
     PC_choise="tijera"
-if (nombre!="piedra") or (nombre!="papel") or (nombre!="tijera"):
-    print("ERROR!")
 
-print(f"Escogiste {nombre}")
+if (nombre=="piedra") or (nombre=="papel") or (nombre=="tijera"):
+    verificacion=True
+else:
+    verificacion=False
+    while verificacion==False:
+        print("ERROR!")
+        nombre=input("por favor,escoge: piedra, papel o tijera => ")
+        if (nombre=="piedra") or (nombre=="papel") or (nombre=="tijera"):
+            verificacion=True
+            break
+
+print(f"\nEscogiste {nombre}")
 print(f"El pc escogio {PC_choise} \n")
 
 if nombre==PC_choise:
