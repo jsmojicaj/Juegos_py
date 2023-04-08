995905bb4022af1812809e038e7702382c079302
Juan Sebastian Mojica <jsmojicaj@unal.edu.co>
2023-04-08 03:31:02 -0500

    Optimización del codigo

--- game_PPT.py
+++ game_PPT.py
@@ -2,24 +2,11 @@ import random
 nombre=input("Escoge: piedra, papel o tijera => ")
 nombre=nombre.lower() #Hace que las mayusculas se vuelvan minusculas. 
 opciones=("piedra","papel","tijera") #tupla de las opciones
-PC=random.randint(1,3)
-if PC==1:
-    PC_choise=opciones[0]
-elif PC==2:
-    PC_choise=opciones[1]
-elif PC==3:
-    PC_choise=opciones[2]
+PC_choise=random.choice(opciones)
 
-if nombre in opciones:
-    verificacion=True
-else:
-    verificacion=False
-    while verificacion==False:
-        print("ERROR!")
-        nombre=input("por favor,escoge: piedra, papel o tijera => ")
-        if nombre in opciones:
-            verificacion=True
-            break
+while nombre not in opciones:
+    print("ERROR!")
+    nombre=input("por favor,escoge: piedra, papel o tijera => ")
 
 print(f"\nEscogiste {nombre}")
 print(f"El pc escogio {PC_choise} \n")
