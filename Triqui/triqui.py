cb83589e1997f9ddc6d1644fd4c22f591901e701
Juan Sebastian Mojica <jsmojicaj@unal.edu.co>
2023-04-16 19:52:43 -0500

    Jugar triqui con movimientos random del PC

--- Triqui/triqui.py
+++ Triqui/triqui.py
@@ -1,34 +1,46 @@
 
 import random
-TEMPLATE_FIELD=[[" "," "," "],[" "," "," "],[" "," "," "]]
+TEMPLATE_FIELD=[" "," "," "," "," "," "," "," "," "]
 TEMPLATE_FIELD_NUMBER = '|0|1|2|\n|3|4|5|\n|6|7|8|\n'
 registro=[]
-for i in range(4):
+resultado="vacio"
+while resultado=="vacio":
     casilla=int(input("Eres la x, Escoge un numero => "))
+    while casilla in registro: casilla=int(input("Ya escogieron esa casilla,escoge un número disponible => "))
+    while (casilla<0) or (casilla>8): casilla=int(input("El numero esta fuera del rango de las casillas, escoga un número dentro del rango 0-8 y que no se haya escogido => "))
     TEMPLATE_FIELD_NUMBER=TEMPLATE_FIELD_NUMBER.replace(str(casilla),"-")
 
-    if casilla<=2: TEMPLATE_FIELD[0][casilla]="x"
-    elif casilla<=5: TEMPLATE_FIELD[1][casilla-3]="x"
-    elif casilla<=8: TEMPLATE_FIELD[2][casilla-6]="x"
-    else: "El numero no esta fuera del rango de las casillas"
+    if casilla<=2: TEMPLATE_FIELD[casilla]="x"
+    elif casilla<=5: TEMPLATE_FIELD[casilla]="x"
+    elif casilla<=8: TEMPLATE_FIELD[casilla]="x"
+
+    triples = [TEMPLATE_FIELD[0:3], TEMPLATE_FIELD[3:6], TEMPLATE_FIELD[6:9], TEMPLATE_FIELD[::3], TEMPLATE_FIELD[1::3],
+            TEMPLATE_FIELD[2::3], [TEMPLATE_FIELD[0],TEMPLATE_FIELD[4],TEMPLATE_FIELD[8]],
+            [TEMPLATE_FIELD[2],TEMPLATE_FIELD[4],TEMPLATE_FIELD[6]]]
+    if ["x","x","x"] in triples: resultado="Tu ganas!"
 
     casilla_pc=int(random.uniform(0,8))
     while (casilla==casilla_pc) or (casilla_pc in registro): 
         casilla_pc=int(random.uniform(0,8))
-        print(casilla_pc)
-    TEMPLATE_FIELD_NUMBER=TEMPLATE_FIELD_NUMBER.replace(str(casilla_pc),"-")
-    if casilla_pc<=2: TEMPLATE_FIELD[0][casilla_pc]="o"
-    elif casilla_pc<=5: TEMPLATE_FIELD[1][casilla_pc-3]="o"
-    elif casilla_pc<=8: TEMPLATE_FIELD[2][casilla_pc-6]="o"
+        if len(registro)>=7: break
 
-    FIELD = f"|{TEMPLATE_FIELD[0][0]}|{TEMPLATE_FIELD[0][1]}|{TEMPLATE_FIELD[0][2]}|\n"\
-            f"|{TEMPLATE_FIELD[1][0]}|{TEMPLATE_FIELD[1][1]}|{TEMPLATE_FIELD[1][2]}|\n"\
-            f"|{TEMPLATE_FIELD[2][0]}|{TEMPLATE_FIELD[2][1]}|{TEMPLATE_FIELD[2][2]}|\n"
+    TEMPLATE_FIELD_NUMBER=TEMPLATE_FIELD_NUMBER.replace(str(casilla_pc),"-")
+    if casilla_pc<=2: TEMPLATE_FIELD[casilla_pc]="o"
+    elif casilla_pc<=5: TEMPLATE_FIELD[casilla_pc]="o"
+    elif casilla_pc<=8: TEMPLATE_FIELD[casilla_pc]="o"
+
+    FIELD = f"|{TEMPLATE_FIELD[0]}|{TEMPLATE_FIELD[1]}|{TEMPLATE_FIELD[2]}|\n"\
+            f"|{TEMPLATE_FIELD[3]}|{TEMPLATE_FIELD[4]}|{TEMPLATE_FIELD[5]}|\n"\
+            f"|{TEMPLATE_FIELD[6]}|{TEMPLATE_FIELD[7]}|{TEMPLATE_FIELD[8]}|\n"
 
     registro.append(casilla)
     registro.append(casilla_pc)
     print(TEMPLATE_FIELD_NUMBER)
     print(FIELD)
-    print(casilla_pc)
+    
+    if ["o","o","o"] in triples: resultado="Gana el PC"
+    if len(registro)>8: resultado="Empate"
+    if resultado != "vacio": print(resultado)
+    
 
 
