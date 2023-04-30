7d19270395c487c1478f714783934305f9693d2f
Juan Sebastian Mojica <jsmojicaj@unal.edu.co>
2023-04-29 22:41:00 -0500

    triqui con inteligencia artificial

--- Triqui/triqui.py
+++ Triqui/triqui.py
@@ -50,34 +50,34 @@ while resultado=="vacio":
     if casilla<=2: TEMPLATE_FIELD[casilla]="x"
     elif casilla<=5: TEMPLATE_FIELD[casilla]="x"
     elif casilla<=8: TEMPLATE_FIELD[casilla]="x"
-
     triples = [TEMPLATE_FIELD[0:3], TEMPLATE_FIELD[3:6], TEMPLATE_FIELD[6:9], TEMPLATE_FIELD[::3], TEMPLATE_FIELD[1::3],
             TEMPLATE_FIELD[2::3], [TEMPLATE_FIELD[0],TEMPLATE_FIELD[4],TEMPLATE_FIELD[8]],
             [TEMPLATE_FIELD[2],TEMPLATE_FIELD[4],TEMPLATE_FIELD[6]]]
     if ["x","x","x"] in triples: resultado="Tu ganas!"
-
-    casilla_pc=int(random.uniform(0,8))
-    while (casilla==casilla_pc) or (casilla_pc in registro): 
-        casilla_pc=int(random.uniform(0,8))
-        if len(registro)>=7: break
+    if resultado != "vacio": print(resultado); break
+    _,casilla_pc=minimax(TEMPLATE_FIELD,8-len(registro),False)
+    print(casilla_pc)
 
     TEMPLATE_FIELD_NUMBER=TEMPLATE_FIELD_NUMBER.replace(str(casilla_pc),"-")
     if casilla_pc<=2: TEMPLATE_FIELD[casilla_pc]="o"
     elif casilla_pc<=5: TEMPLATE_FIELD[casilla_pc]="o"
     elif casilla_pc<=8: TEMPLATE_FIELD[casilla_pc]="o"
-
+    
     FIELD = f"|{TEMPLATE_FIELD[0]}|{TEMPLATE_FIELD[1]}|{TEMPLATE_FIELD[2]}|\n"\
             f"|{TEMPLATE_FIELD[3]}|{TEMPLATE_FIELD[4]}|{TEMPLATE_FIELD[5]}|\n"\
             f"|{TEMPLATE_FIELD[6]}|{TEMPLATE_FIELD[7]}|{TEMPLATE_FIELD[8]}|\n"
-
+    
     registro.append(casilla)
     registro.append(casilla_pc)
     print(TEMPLATE_FIELD_NUMBER)
     print(FIELD)
-    
+    triples = [TEMPLATE_FIELD[0:3], TEMPLATE_FIELD[3:6], TEMPLATE_FIELD[6:9], TEMPLATE_FIELD[::3], TEMPLATE_FIELD[1::3],
+            TEMPLATE_FIELD[2::3], [TEMPLATE_FIELD[0],TEMPLATE_FIELD[4],TEMPLATE_FIELD[8]],
+            [TEMPLATE_FIELD[2],TEMPLATE_FIELD[4],TEMPLATE_FIELD[6]]]
     if ["o","o","o"] in triples: resultado="Gana el PC"
-    if len(registro)>8: resultado="Empate"
-    if resultado != "vacio": print(resultado)
+    if len(registro)>8: resultado="Empate"   
+    
+    if resultado != "vacio": print(resultado);break
 
 
 
